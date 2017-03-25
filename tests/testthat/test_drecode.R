6acdde4b00c9a4e8a11eaa1361aa9a9c11da70a8
diegogarcilazo <diego_garcilazo@yahoo.com.ar>
2017-03-25 01:13:02 -0300

    update test_drecode

--- tests/testthat/test_drecode.R
+++ tests/testthat/test_drecode.R
@@ -1,15 +1,9 @@
 library(myutilities)
 testthat::context("drecode")
-
 #Test recode
-
 testthat::test_that("drecode return a data frame", {
 df = tibble::data_frame(a = 1:18, b = LETTERS[1:18])
-result_expected <- structure(list(a = 1:18, b = c("A", "B", "C", "D", "E", "F", 
-                                                  "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"), prueba = c(0.12, 0.1, 0.09, 0.09, 0.08, 0.08, 0.06, 0.06, 
-                                                                   0.06, 0.06, 0.05, 0.04, 0.04, 0.03, 0.02, 0.01, 0.005, 0.005, 
-                                                                   NA, NA)), row.names = c(NA, -18L), .Names = c("a", "b", "prueba"
-                                                                   ), class = c("tbl_df", "tbl", "data.frame"))
+result_expected <- structure(list(1:18, c("A", "B", "C", "D", "E", "F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"), c(0.12, 0.1, 0.09, 0.09, 0.08, 0.08, 0.06, 0.06,0.06, 0.06, 0.05, 0.04, 0.04, 0.03, 0.02, 0.01, 0.005, 0.005)), row.names = c(NA, -18L), .Names = c("a", "b", "prueba"), class = c("tbl_df", "tbl", "data.frame"))
 
 result <- drecode(df, myutilities::gedadesn18, a, age.group, W, prueba)
 
