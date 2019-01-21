f4c53c9f0975abbc4e3df03998fe5e46758eb577
diegogarcilazo <diego_garcilazo@yahoo.com.ar>
2019-01-21 14:49:19 -0300

    modify classint function

--- R/classint.R
+++ R/classint.R
@@ -3,17 +3,24 @@
 #' @param n int: number of intervals.
 #' @param style chr: breaks style. chosen style: one of "fixed", "sd", "equal", "pretty", "quantile", "kmeans", "hclust", "bclust", "fisher", or "jenks". Default: "quantile".
 #' @param labels: chr vector with labels for class.
-#' @param include.lowest: logical, indicating if an equal to the lowest
-#' @return factor. 
+#' @param include.lowest: logical, indicating if an ???x[i]??? equal to the lowest
+#' @param brks: vector of the same length of n with breaks numbers. Work in the same way that argument breaks from cut function.
+#'
 
-classint <- function(var, n, style = 'quantile', labels = NULL, include.lowest = T){
+classint <- function(var, n, style = 'quantile', labels = NULL, include.lowest = T, brks = NULL){
   
+  if(is.null(brks)){
+    
     ci_obj <- classInt::classIntervals(var, n = n,
                                        style = style);
     brks <- unique(ci_obj$brks);
     
-    vect_class <- cut(var, brks, labels = labels, 
-                      include.lowest = include.lowest);
+  }else{
+    stopifnot(length(brks)-1 == n);
+    brks <- brks;
+  }
+  
+  vect_class <- cut(var, brks, labels = labels, include.lowest = include.lowest);
   
   return(vect_class);
 }
--- developer.R
+++ developer.R
@@ -18,6 +18,7 @@ givesWarning <- function(expr){
 }
 
 
+
 # import export json
 my_list = list(a, b, dft)
 
@@ -27,3 +28,31 @@ write(jsonlite::toJSON(my_list, pretty = T), 'my_json.json')
 
 readlines <- readLines(file('my_json.json'), warn = FALSE)
 parse <- jsonlite::fromJSON(readlines)
+
+
+library(tidyverse)
+
+
+#valuey
+year <- 136
+
+var <- rlang::sym("mass")
+
+birth_year <- starwars$mass == 136
+
+filter(starwars, birth_year)
+
+#namey
+new_var <- "myname"
+
+summarise(starwars, !!new_var := sum(!!var, na.rm = T))
+
+
+t <- starwars %>% mutate(
+  l = height - mass
+)
+
+t %>% mutate(
+  f = map2(if_else(is.na(mass), 1, mass), ~ rep(0,.x))) %>% 
+  select(l, f, mass)
+
